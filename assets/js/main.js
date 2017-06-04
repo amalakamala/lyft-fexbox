977dd27b6e82430542fbf41f3293d466991d0f51
amalakamala <amalakamala.a@gmail.com>
2017-06-03 20:03:42 -0400

    SIGN UP

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,23 +4,45 @@ function resizeHeaderOnScroll() {
   window.pageYOffset:  devuelve los píxeles del documento actual.
   scrollTop : devuelve el número de píxeles contenido de un elemento se desplaza verticalmente.
   */
-  var limite = 300;
+  var limite = 250;
   var elNav = document.getElementById('navegador'); 
   var elHide = document.getElementById('a-hide');
   var lasA = Array.from(document.getElementsByClassName('estilo-a'));
+  var navEspecial = document.getElementById('log-in');
+  var logoW = document.getElementById('logo-white');
+  var logoP = document.getElementById('logo-pink');
+
 
   if (distanciaY > limite) {
-    elNav.classList.add("menu-dos");  
+    elNav.classList.add("menu-dos"); 
+
     elHide.classList.remove("nav-hide");
+    elHide.classList.add("estilo-a-scroll"); 
+
     lasA.forEach(function(e){
       e.classList.add("color-a"); 
     });
+    navEspecial.classList.remove("nav-especial");
+    navEspecial.classList.add("nav-especial-scroll");
+
+    logoP.classList.remove("hide");    
+    logoW.classList.add("hide");
+    logoP.classList.add("logo");
+
   }else{
     elNav.classList.remove("menu-dos");
+
     elHide.classList.add("nav-hide")
+
     lasA.forEach(function(e){
       e.classList.remove("color-a"); 
     }); 
+    navEspecial.classList.add("nav-especial");
+
+    logoP.classList.add("hide");
+    logoW.classList.remove("hide");  
+    logoP.classList.remove("logo");
+
   }
 }
 
